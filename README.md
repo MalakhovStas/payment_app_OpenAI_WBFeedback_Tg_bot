c0fa709a16c027e591f351369d8a81db8a1500a2
MalakhovStas <ff9147782@gmail.com>
2023-04-17 20:56:18 +0300

    add method post for multipart/form-data

--- README.md
+++ README.md
@@ -25,7 +25,26 @@ curl -X 'POST' \
   'http://127.0.0.1:8000/' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
-  -d '{"user_id": "1438292311", "add_to_balance": 15}'
+  -d '{ "date":"2023-04-17T19:53:10+03:00",
+        "order_id":"11665981",
+        "order_num":"1",
+        "domain":"marpla.payform.ru",
+        "sum":"300.00","currency":"rub",
+        "customer_phone":"",
+        "customer_email":"super.nvhelp@yandex.ru",
+        "customer_extra":"",
+        "payment_type":"\u041e\u043f\u043b\u0430\u0442\u0430 \u043a\u0430\u0440\u0442\u043e\u0439, \u0432\u044b\u043f\u0443\u0449\u0435\u043d\u043d\u043e\u0439 \u0432 \u0420\u0424",
+        "commission":"3.5",
+        "commission_sum":"10.50",
+        "attempt":"1",
+        "_param_user_id":"1438292311",
+        "products":[{"name":"\u0417\u0430\u043f\u0440\u043e\u0441",
+                     "price":"10.00",
+                     "quantity":"30",
+                     "sum":"300.00"}],
+        "payment_status":"success",
+        "payment_status_description":"\u0423\u0441\u043f\u0435\u0448\u043d\u0430\u044f \u043e\u043f\u043b\u0430\u0442\u0430",
+        "payment_init":"manual"}'
 
 
   -H 'Content-type:text/plain;charset=utf-8' \
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, File
 from peewee import SqliteDatabase
 from config import *
 from loguru import logger
-
+from typing import Annotated
 
 logger.add(**LOGGER_ERRORS)
 logger.add(**LOGGER_DEBUG)
@@ -26,17 +26,24 @@ def update_user_balance_requests(add_to_balance, user_id):
 app = FastAPI()
 
 
-@app.post('/')
+@app.post('/json')
 def router_post_update(data: dict) -> dict:
     logger.warning(f'incoming request -> method POST {data=}')
-    user_id = data.get('user_id')
-    add_to_balance = data.get('add_to_balance')
 
+    user_id = data.get('_param_user_id')
+    add_to_balance = data.get('products')[0].get('quantity')
+    print(user_id, add_to_balance)
     update_data = update_user_balance_requests(add_to_balance=add_to_balance, user_id=user_id)
 
     if update_data.get('error'):
         logger.warning(str(update_data.get('error')))
     return update_data
+
+
+@app.post('/form_data')
+def router_post_update(file: Annotated[bytes, File()]) -> dict:
+    logger.warning(f'incoming request -> method POST -> multipart/form-data {file}')
+    return {"file_size": len(file)}
 
 
 @app.get('/')
@@ -46,3 +53,25 @@ def router_get_update() -> dict:
 
 # import subprocess
 # subprocess.run('uvicorn main:app --reload')
+
+"""{
+"date":"2023-04-17T19:53:10+03:00",
+"order_id":"11665981",
+"order_num":"1",
+"domain":"marpla.payform.ru",
+"sum":"300.00","currency":"rub",
+"customer_phone":"",
+"customer_email":"super.nvhelp@yandex.ru",
+"customer_extra":"",
+"payment_type":"\u041e\u043f\u043b\u0430\u0442\u0430 \u043a\u0430\u0440\u0442\u043e\u0439, \u0432\u044b\u043f\u0443\u0449\u0435\u043d\u043d\u043e\u0439 \u0432 \u0420\u0424",
+"commission":"3.5",
+"commission_sum":"10.50",
+"attempt":"1",
+"_param_user_id":"1438292311",
+"products":[{"name":"\u0417\u0430\u043f\u0440\u043e\u0441",
+             "price":"10.00",
+             "quantity":"30",
+             "sum":"300.00"}],
+"payment_status":"success",
+"payment_status_description":"\u0423\u0441\u043f\u0435\u0448\u043d\u0430\u044f \u043e\u043f\u043b\u0430\u0442\u0430",
+"payment_init":"manual"}"""
